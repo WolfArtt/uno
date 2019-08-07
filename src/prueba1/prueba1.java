779321b047be30501b711713a87240c93d05a358
Pedro Arteaga <pedregales5@gmail.com>
2019-08-07 13:08:18 -0400

    prueba de hola y una suma

--- src/prueba1/prueba1.java
+++ src/prueba1/prueba1.java
@@ -4,6 +4,9 @@ public class prueba1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hola kike");
+		System.out.println("hola a todos");
+		int a=5,b=3,c=0;
+		c=a+b;
+		System.out.println("la suma es:  "+c);
 	}
 }
